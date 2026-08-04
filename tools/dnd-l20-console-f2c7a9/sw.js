fd15ff54157915f7fc46fa641bcd83bb1ec21b21
Ramie Fathy <ramiefathy@gmail.com>
2026-08-03 23:07:43 -0400

    Bump D&D service-worker cache

--- tools/dnd-l20-console-f2c7a9/sw.js
+++ tools/dnd-l20-console-f2c7a9/sw.js
@@ -1,4 +1,4 @@
-const CACHE='table-ledger-4.0.0-srd521';
+const CACHE='table-ledger-4.0.1-ci-remediation';
 const ASSETS=['./','index.html','styles.css','data.js','app.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
